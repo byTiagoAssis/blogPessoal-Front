e01a727a4de8e18a4154cce65bac21beab2d39e0
byTiagoAssis <tiagoassis0594@gmail.com>
2021-11-10 19:09:53 -0300

    Acrescentando service tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../Model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,8 +11,12 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -18,6 +24,22 @@ export class TemaComponent implements OnInit {
      
       this.router.navigate(['/entrar'])
     }
+    this.findAllTema()
   }
 
+findAllTema (){
+
+  this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+    this.listaTemas= resp
+  })
+}
+
+cadastrar () {
+  this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+    this.tema= resp
+    alert('Tema cadastrado com sucesso')
+    this.findAllTema
+    this.tema = new Tema()
+  })
+}
 }
